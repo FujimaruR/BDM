c64e3a2a598bd03490dacf221e42908a787ff46e
OhKaiitsKai <aymeealvarezv@gmail.com>
2023-08-31 01:28:20 -0600

    Landing page.

---
+++ assets/footer.php
@@ -0,0 +1,9 @@
+<footer class="py-3 navbar-dark bg-dark mt-auto">
+    <div class="container">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="./main.php" class="nav-link px-2 text-muted">Inicio</a></li>
+            <li class="nav-item"><a href="./index.php" class="nav-link px-2 text-muted">Sobre nosotros</a></li>
+        </ul>
+        <p class="text-center text-muted">&copy; 2022 LMAD Company, Inc</p>
+    </div>
+</footer>
